dbf3d3d5fa39c9c67c69ab8fd554a43c6a47ba78
Aishwarya Nirmal <anirmal@attu2.cs.washington.edu>
2018-02-04 00:48:02 -0800

    sending to local

--- ppdbmodel.py
+++ ppdbmodel.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('/homes/iws/anirmal/research/research-nlp-2018/')
 import paraphraseAgePow as ppdb
 #from gensim.models.KeyedVectors import KeyedVectors
-import gensim as gs
+# import gensim as gs
 
 '''
 TODO 
@@ -13,8 +13,8 @@ TODO
 '''
 
 class PPDBModel():
-  def __init__(self):
-    self.vectors = gs.models.KeyedVectors.load_word2vec_format('/homes/iws/anirmal/research/research-nlp-2018/retrofitted_vecs.txt', binary=False)
+  # def __init__(self):
+  # self.vectors = gs.models.KeyedVectors.load_word2vec_format('/homes/iws/anirmal/research/research-nlp-2018/retrofitted_vecs.txt', binary=False)
 
   def getOutput(self, inputStr, dimension, change):
     tokens = inputStr.split()
@@ -26,15 +26,22 @@ class PPDBModel():
       else:
         print(val)
         paras = list(val.keys())
-        similar_words = []
+        similar_word = None
         if change == 'increase':
+          max = -1
           for para in paras:
-            if float(val[para]) > 0.0:
-              similar_words.append(para)
-        if len(similar_words) == 0:
+            if float(val[para]) > max:
+              similar_word = para
+        else:
+          min = 1
+          for para in paras:
+            if float(val[para]) < min:
+              similar_word = para
+
+        if similar_word == None:
           paraphrase.append(token)
         else:
-          most_similar = self.vectors.most_similar_to_given(token, similar_words)
-          paraphrase.append(most_similar)
+          # most_similar = self.vectors.most_similar_to_given(token, similar_words)
+          paraphrase.append(similar_word)
     return " ".join(paraphrase)
 
